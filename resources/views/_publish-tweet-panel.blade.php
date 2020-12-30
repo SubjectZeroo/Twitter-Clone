bde2f797335aedee40c32821de3e2336557ef3d5
Jafeht <jasordev@gmail.com>
2020-12-29 23:54:32 -0400

    empezando con el front end con tailwindcss

---
+++ resources/views/_publish-tweet-panel.blade.php
@@ -0,0 +1,19 @@
+<div class="border border-blue-400 rounded-lg px-8 py-6 mb-5">
+    <form action="">
+        <textarea
+        name="body"
+        class="w-full"
+        placeholder="What's up doc"></textarea>
+        <hr class="my-4">
+
+        <footer class="flex justify-between">
+            <img
+            src="https://picsum.photos/40"
+            alt=""
+            class="rounded-full mr-2"
+            >
+            <button class="bg-blue-500 rounded-lg shadow py-2 px-2 text-white" type="submit">Tweet a root</button>
+        </footer>
+    </form>
+
+</div>
---
+++ resources/views/_tweet.blade.php
@@ -0,0 +1,13 @@
+<div class="flex p-4 border-b border-b-gray-400">
+    <div class="mr-2 flex-shrink-0">
+        <img
+        src="https://picsum.photos/50"
+        alt=""
+        class="rounded-full mr-2"
+        >
+    </div>
+    <div>
+        <h5 class="font-bold mb-4">Jhon Doe</h5>
+        <p class="text-sm"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima debitis maiores itaque, quasi dolore ratione architecto ea temporibus cumque quisquam ipsa alias fugit distinctio eveniet, rem at nisi autem vel.</p>
+    </div>
+</div>
